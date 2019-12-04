2eab0d5e6662a3ce66d80c36238f8fcb09a6ac9c
Rimma <rim23ma@gmail.com>
2019-12-03 21:44:40 -0600

    header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rimma Akhmedyanova - Software Developer`,
+    description: `Welcome to my personal portfolio! Come and see who I am, what I did and what I can do. Check out my Contacts section to contact for any enquiries.`,
+    author: `@rimmaakhmedyanova`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
